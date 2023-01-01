a9453edeaf65dfcd9a0d047da3e02294148897da
Xavier Hernandez <xhernan.martinez@gmail.com>
2023-01-01 15:17:27 +0100

    comment - content to integrate from original HRNet

--- src/views/List.tsx
+++ src/views/List.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "./List.module.scss";
 
+// display by 10 / 25 / 50 / 100
+// search => display one
+// firsname, lastname, stardatae, departement, dob, street, city, state, zipcode
+// all sortable
+// pagination possible
+
 const List: React.FC = () => {
   return <section></section>;
 };
--- src/views/Subscription.tsx
+++ src/views/Subscription.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import styles from "./Subscription.module.scss";
 
+// Employee
+// text FirstName
+// text LastName
+// date DoB
+// date StartDate
+
+// Address
+// text street
+// text city
+// select State (US)
+// number zipcode
+
+// Department
+// Select Department (sales, marketing, engineering, human resources, legal)
+
 const Subscription: React.FC = () => {
   return <section></section>;
 };
